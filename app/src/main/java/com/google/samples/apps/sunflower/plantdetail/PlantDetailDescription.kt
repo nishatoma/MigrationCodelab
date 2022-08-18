87085e28306e277573feea988d025b6ec6f9e25b
Nisha <nishatoma@gmail.com>
2022-08-17 20:39:25 -0400

    Add HTML description

--- app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,6 +16,8 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -27,6 +29,7 @@ import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -34,6 +37,8 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
 import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
@@ -54,6 +59,7 @@ private fun PlantDetailContent(plant: Plant) {
     Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.margin_normal))) {
         PlantName(name = plant.name)
         PlantWatering(wateringInterval = plant.wateringInterval)
+        PlantDescription(description = plant.description)
     }
 }
 
@@ -88,6 +94,34 @@ fun PlantWatering(wateringInterval: Int) {
         // Watering interval text
         Text(text = wateringIntervalText,
         modifier = centerWithPaddingModifier.padding(bottom = normalPadding))
+    }
+}
+
+@Composable
+private fun PlantDescription(description: String) {
+    // Remember the HTML formatted description
+    val htmlDesc = remember(description) {
+        HtmlCompat.fromHtml(description, HtmlCompat.FROM_HTML_MODE_COMPACT)
+    }
+
+    // Used AndroidView to display Spannable objects and HTML
+    AndroidView(
+        factory = { context ->
+            TextView(context).apply {
+                movementMethod = LinkMovementMethod.getInstance()
+            }
+        },
+        update = {
+            it.text = htmlDesc
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun PlantDescriptionPreview() {
+    MaterialTheme {
+        PlantDescription("HTML<br><br>description")
     }
 }
 
